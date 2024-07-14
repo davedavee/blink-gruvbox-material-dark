e5071be38baa6478dc3500e873a370543cf9f56e
František Věříš <davedavee@users.noreply.github.com>
2024-07-14 14:50:00 +0200

    Created gruvbox-material-dark.js

---
+++ gruvbox-material-dark.js
@@ -0,0 +1,26 @@
+black       = '#32302F'; // bg
+red         = '#472322'; // red
+green       = '#3D4220'; // green
+yellow      = '#574833'; // yellow
+blue        = '#0F3A42'; // blue
+magenta     = '#543937'; // purple
+cyan        = '#424A3E'; // aqua
+white       = '#A89984'; // light gray
+lightBlack  = '#928374'; // medium gray
+lightRed    = '#EA6962'; // red
+lightGreen  = '#A9B665'; // green
+lightYellow = '#D8A657'; // yellow
+lightBlue   = '#7DAEA3'; // blue
+lightMagenta= '#D3869B'; // purple
+lightCyan   = '#89B482'; // aqua
+lightWhite  = '#D4BE98'; // fg
+ 
+t.prefs_.set('color-palette-overrides',
+                 [ black ,red, green,yellow,
+                  blue, magenta, cyan, white,
+                  lightBlack, lightRed, lightGreen, lightYellow,
+                  lightBlue ,lightMagenta, lightCyan, lightWhite ]);
+ 
+t.prefs_.set('cursor-color', '#FE8019');
+t.prefs_.set('foreground-color', lightWhite);
+t.prefs_.set('background-color', black);
